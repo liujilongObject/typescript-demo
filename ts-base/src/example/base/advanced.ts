1142fa1a391e29fc463f7d237f84ab99d9a85563
liujilong <18331508618@163.com>
2020-06-03 11:49:11 +0800

    feat: 联合类型

--- ts-base/src/example/base/advanced.ts
+++ ts-base/src/example/base/advanced.ts
@@ -22,3 +22,34 @@ let a: number | string = 'a'
 let testB: 'a' | 'b' | 'c'
 testB = 'a'
 // testB = 'ff' // error
+
+
+interface Square {
+  kind: 'square'
+  size: number
+}
+interface Rectangle {
+  kind: 'rectangle'
+  width: number
+  height: number
+}
+interface Circle {
+  kind: 'circle'
+  r: number
+}
+type Shape = Square | Rectangle | Circle
+
+function area(s: Shape) {
+  switch(s.kind) {
+    case 'square':
+      return s.size ** 2
+    case 'rectangle':
+      return s.width * s.height
+    case 'circle':
+      return Math.PI * s.r ** 2
+    default:
+      return ((e: never) => {throw new Error(e)})(s)
+  }
+}
+
+console.log(area({ kind: 'circle', r: 1 }))
