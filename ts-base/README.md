cace9ffb80a87ed0eb1d3e17a45bbd762151b8b0
liujilong <18331508618@163.com>
2020-06-03 15:04:24 +0800

    feat: 高级类型-映射类型&条件类型

--- ts-base/README.md
+++ ts-base/README.md
@@ -1,1 +1,3 @@
 #### TypeScript的学习笔记
+
+**[相关资源](https://github.com/geektime-geekbang/typescript-in-action)**
--- ts-base/src/example/base/advanced.ts
+++ ts-base/src/example/base/advanced.ts
@@ -77,3 +77,51 @@ const testObj = {
 }
 console.log(getValues(testObj, ['a', 'b']))
 // console.log(getValues(testObj, ['e', 'f'])) // error
+
+
+/**
+ * 映射类型
+ * 本质上是预先定义的泛型接口，通常会结合索引类型获取对象的属性或属性值，来讲一个对象映射成我们想要的结构
+ */
+interface MapObj {
+  a: string
+  b: number
+  c: boolean
+}
+// ts 内置映射类型
+type ReadonlyMapObj = Readonly<MapObj>
+
+type PickMapObj = Pick<MapObj, 'a' | 'c'>
+
+type RecordMapObj = Record<'x' | 'y', MapObj>
+
+
+/**
+ * 条件类型
+ * T extends U ? X : Y
+ */
+type TypeName<T> = 
+  T extends string ? 'string' :
+  T extends number ? 'number' :
+  T extends boolean ? 'boolean' :
+  T extends undefined ? 'undefined' :
+  T extends Function ? 'function' :
+  'object';
+
+type T1 = TypeName<string>
+type T2 = TypeName<string[]>
+
+// (A | B) extends U ? X : Y
+// (A extends U ? X : Y) | (B extends U ? X : Y)
+type T3 = TypeName<string | number>
+
+type Diff<T, U> = T extends U ? never : T
+
+/* ts 内置条件类型 */
+// Extract from T those types that are assignable to U
+type T4 = Extract<'a' | 'b' | 'c', 'a' | 'e'>
+// Exclude from T those types that are assignable to U
+type T5 = Exclude<'a' | 'b' | 'c', 'a' | 'e'>
+
+// ReturnType<T>
+type T6 = ReturnType<() => string | number>
