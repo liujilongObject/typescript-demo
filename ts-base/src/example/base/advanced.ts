89e0dc98f56415301c1d56cef2cd323041621341
liujilong <18331508618@163.com>
2020-06-03 12:40:20 +0800

    feat: 高级类型-索引类型

--- ts-base/src/example/base/advanced.ts
+++ ts-base/src/example/base/advanced.ts
@@ -53,3 +53,27 @@ function area(s: Shape) {
 }
 
 console.log(area({ kind: 'circle', r: 1 }))
+
+/**
+ * 索引类型
+ */
+// keyof T
+interface Obj {
+  a: number,
+  b: string
+}
+let key: keyof Obj
+
+// T[k]
+let value: Obj['a']
+
+function getValues<T, K extends keyof T>(obj: T, keys: K[]): T[K][] {
+  return keys.map(key => obj[key])
+}
+const testObj = {
+  a: 1,
+  b: 2,
+  c: 3
+}
+console.log(getValues(testObj, ['a', 'b']))
+// console.log(getValues(testObj, ['e', 'f'])) // error
